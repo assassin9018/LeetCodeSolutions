89cdc9145304300a24e2b0ced6363ae53418eceb
beskaravaev.a <34675794+assassin9018@users.noreply.github.com>
2022-04-01 13:32:25 +0300

    Create S0050.cs

---
+++ LeetCode/Solutions/Medium/S0050.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Solutions.Medium;
+
+internal class S0050 : IIssueSolution
+{
+    public int Number => 50;
+
+    public string Name => "Pow(x, n)";
+
+    public void Run()
+    {
+        double x = ReadDouble();
+        int n = ReadInt();
+        Console.WriteLine(MyPow(x, n));
+    }
+    private double MyPow(double x, int n)
+    {
+        double value = Math.Abs(x);
+        double sign = (n & 1) == 0 ? 1 : Math.Sign(x);
+        double result = Math.Exp(n * Math.Log(value));
+
+        return result * sign;
+    }
+
+    public string Decription =>
+@"Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+Constraints:
+
+-100.0 < x < 100.0
+-2^31 <= n <= 2^31-1
+-10^4 <= xn <= 10^4";
+}
