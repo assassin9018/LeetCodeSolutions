5bf23eabfc93b411410383f7001c2840f1a6bc1e
beskaravaev.a <34675794+assassin9018@users.noreply.github.com>
2022-06-07 14:41:42 +0300

    Fix refactoring messages

--- LeetCode/Solutions/EnumerableResultSolution.cs
+++ LeetCode/Solutions/EnumerableResultSolution.cs
@@ -1,20 +1,19 @@
-﻿namespace LeetCode.Solutions
+﻿namespace LeetCode.Solutions;
+
+public abstract class EnumerableResultSolution<T> : SolutionBase<IEnumerable<T>>
 {
-    public abstract class EnumerableResultSolution<T> : SolutionBase<IEnumerable<T>>
+    private protected readonly IWriteHelper _writer;
+
+    protected EnumerableResultSolution()
     {
-        private protected readonly IWriteHelper _writer;
-
-        protected EnumerableResultSolution()
-        {
-            _writer = new ConsoleWriteHelper();
-        }
-
-        protected EnumerableResultSolution(IReadHelper reader) : base(reader)
-        {
-            _writer = new ConsoleWriteHelper();
-        }
-
-        private protected override void PrintResult(IEnumerable<T> result)
-            => _writer.Write(result);
+        _writer = new ConsoleWriteHelper();
     }
+
+    protected EnumerableResultSolution(IReadHelper reader) : base(reader)
+    {
+        _writer = new ConsoleWriteHelper();
+    }
+
+    private protected override void PrintResult(IEnumerable<T> result)
+        => _writer.Write(result);
 }
--- LeetCode/WriteHelper.cs
+++ LeetCode/WriteHelper.cs
@@ -1,17 +1,16 @@
-﻿namespace LeetCode
+﻿namespace LeetCode;
+
+internal class ConsoleWriteHelper : IWriteHelper
 {
-    internal class ConsoleWriteHelper : IWriteHelper
-    {
-        public void Write<T>(IEnumerable<T> arr)
-            => Console.WriteLine(String.Join(' ', arr.Select(x => x?.ToString() ?? "null")));
+    public void Write<T>(IEnumerable<T> arr)
+        => Console.WriteLine(String.Join(' ', arr.Select(x => x?.ToString() ?? "null")));
 
-        public void Write<T>(T value)
-            => Console.WriteLine(value?.ToString());
-    }
+    public void Write<T>(T value)
+        => Console.WriteLine(value?.ToString());
+}
 
-    public interface IWriteHelper
-    {
-        void Write<T>(IEnumerable<T> arr);
-        void Write<T>(T value);
-    }
+public interface IWriteHelper
+{
+    void Write<T>(IEnumerable<T> arr);
+    void Write<T>(T value);
 }
