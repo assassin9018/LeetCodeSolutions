73193ee15840cf0c20bc798913aeb3ad1cccd47c
beskaravaev.a <34675794+assassin9018@users.noreply.github.com>
2022-06-09 14:38:10 +0300

    Added project for MS tests

---
+++ LeetCodeTests/Helpers/TestReadHelper.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Helpers;
+
+namespace LeetCodeTests.Helpers;
+
+internal class TestReadHelper : BaseReadHelper
+{
+    private readonly string[] _valuesForTest;
+    private int _iterator;
+
+    public TestReadHelper(string[] valuesForTest)
+    {
+        _valuesForTest = valuesForTest;
+    }
+
+    protected override string GetStringForParsing()
+        => _valuesForTest[_iterator++];
+
+    protected override void PringMessage(string message)
+    {
+    }
+}
---
+++ LeetCodeTests/Helpers/TestWriteHelper.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Helpers;
+
+namespace LeetCodeTests.Helpers;
+
+internal class TestWriteHelper<T> : IWriteHelper<T>
+{
+    public T? LastValue { get; private set; }
+    public IEnumerable<T>? LastValues { get; private set; }
+
+    public void Write(IEnumerable<T> values)
+        => LastValues = values;
+
+    public void Write(T value)
+        => LastValue = value;
+}
