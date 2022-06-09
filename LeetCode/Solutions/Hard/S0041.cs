afb488320085a9d508c79b33bcd9b402acb4856d
beskaravaev.a <34675794+assassin9018@users.noreply.github.com>
2022-06-09 14:38:47 +0300

    Create S0041

---
+++ LeetCode/Solutions/Hard/S0041.cs
@@ -0,0 +1,98 @@
+﻿using LeetCode.Helpers;
+
+namespace LeetCode.Solutions.Hard;
+public class S0041 : SingleResultSolution<int>
+{
+    public S0041()
+    {
+    }
+
+    public S0041(IReadHelper reader, IWriteHelper<int> writer) : base(reader, writer)
+    {
+    }
+
+    public override int Number => 41;
+
+    public override string Name => "First Missing Positive";
+
+    private protected override Func<int> CreateExecutionMethod()
+    {
+        int[] arr = _reader.ReadArray("Array from any to any value with one missed positive number.");
+
+        return () => FirstMissingPositive(arr);
+    }
+
+    private static int FirstMissingPositive(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int n = nums[i];
+            if (n > 0 && n != i + 1 && n < nums.Length)
+            {
+                int ti = nums[i] = nums[n - 1];
+                nums[n - 1] = n;
+                if (ti!=n && ti - 1 < i)
+                    i--;
+            }
+        }
+        for (int i = 0; i < nums.Length; i++)
+            if (nums[i] != i + 1)
+                return i + 1;
+
+        return nums.Length + 1;
+    }
+
+    private static int FirstMissingPositiveJavaStolen(int[] nums)
+    {
+        bool one = false;
+
+        //step 1 
+        for (int i = 0; i < nums.Length; i++) //put in range means 1<=nums<=n
+        {
+            if (nums[i] == 1)
+                one = true;
+            if (!(nums[i] >= 1 && nums[i] <= nums.Length))
+                nums[i] = 1;
+        }
+        // System.out.println(one);
+        if (one == false)
+            return 1;   // because 1 is not present 
+                        // System.out.println(Arrays.toString(nums));
+                        // step 2 --> put it coresponding indexes to minus
+
+        for (int i = 0; i < nums.Length; i++)
+            nums[Math.Abs(nums[i]) - 1] = -Math.Abs(nums[Math.Abs(nums[i]) - 1]);
+
+
+        //step 3 iterate over array again and see which index is not positive
+
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+                return i + 1;
+        }
+
+
+        return nums.Length + 1;
+    }
+
+    public override string Decription => @"Given an unsorted integer array nums, return the smallest missing positive integer.
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+
+Example 1:
+Input: nums = [1,2,0]
+Output: 3
+
+Example 2:
+Input: nums = [3,4,-1,1]
+Output: 2
+
+Example 3:
+Input: nums = [7,8,9,11,12]
+Output: 1
+
+Constraints:
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1";
+}
---
+++ LeetCodeTests/SolutionTests/Hard/S0041Test.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Solutions.Hard;
+using LeetCodeTests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTests.SolutionTests.Hard;
+
+[TestClass]
+public class S0041Test
+{
+    [TestMethod]
+    [DataRow("0,1,2,3,4,5,6,8", 7)]
+    [DataRow("1,2,3,4,5,6,8", 7)]
+    [DataRow("-1,-2,-3,-10,1,2,3,4,5,6,8", 7)]
+    [DataRow("0,2,3,4,5,6,7,8", 1)]
+    [DataRow("6,7,8,9", 1)]
+    [DataRow("1,1", 2)]
+    [DataRow("1", 2)]
+    [DataRow("-1,4,2,1,9,10", 3)]
+    public void TestMethod1(string input, int expected)
+    {
+        TestReadHelper reader = new(new[] { input });
+        TestWriteHelper<int> writer = new();
+        S0041 solver = new(reader, writer);
+        solver.Run();
+        Assert.AreEqual(expected, writer.LastValue);
+    }
+}
