4021d92b7c58c365a23a881c7c26f05009ea1400
Beskaravaev.A <34675794+assassin9018@users.noreply.github.com>
2025-01-25 01:17:46 +0300

    solve S0802

---
+++ LeetCode/Solutions/Medium/S0802.cs
@@ -0,0 +1,87 @@
+﻿using LeetCode.Helpers;
+using System.Collections;
+
+namespace LeetCode.Solutions.Medium;
+
+internal class S0802(IReadHelper reader, IWriteHelper<int> writer) : EnumerableResultSolution<int>(reader, writer)
+{
+    public override int Number => 802;
+    public override string Name => "Find Eventual Safe States";
+
+    private protected override Func<IEnumerable> CreateExecutionMethod()
+    {
+        var graph = _reader.Read2Array("Input graph");
+
+        return () => EventualSafeNodes(graph);
+    }
+
+    public static IList<int> EventualSafeNodes(int[][] graph)
+    {
+        var safeNodes = new HashSet<int>();
+        var visitedNodes = new HashSet<int>();
+
+        for (int i = 0; i < graph.Length; i++)
+            IsSafeNode(graph, i, safeNodes, visitedNodes); // DFS
+
+        return [.. safeNodes.Order()];
+    }
+
+    private static bool IsSafeNode(int[][] graph, int index, HashSet<int> safeNodes, HashSet<int> visitedNodes)
+    {
+        var edges = graph[index];
+
+        if (edges.Length == 0)
+        {
+            safeNodes.Add(index);
+            visitedNodes.Add(index);
+            return true;
+        }
+
+        if (safeNodes.Contains(index))
+            return true;
+
+        if (visitedNodes.Contains(index))
+            return false;
+
+        visitedNodes.Add(index);
+        if (edges.All(edge => IsSafeNode(graph, edge, safeNodes, visitedNodes)))
+        {
+            safeNodes.Add(index);
+            return true;
+        }
+
+        return false;
+    }
+
+    public override string Decription
+        =>
+        """
+        There is a directed graph of n nodes with each node labeled from 0 to n - 1. The graph is represented by a 0-indexed 2D integer array graph where graph[i] is an integer array of nodes adjacent to node i, meaning there is an edge from node i to each node in graph[i].
+        A node is a terminal node if there are no outgoing edges. A node is a safe node if every possible path starting from that node leads to a terminal node (or another safe node).
+        Return an array containing all the safe nodes of the graph. The answer should be sorted in ascending order.
+        
+        Example 1:
+        Illustration of graph
+        Input: graph = [[1,2],[2,3],[5],[0],[5],[],[]]
+        Output: [2,4,5,6]
+        Explanation: The given graph is shown above.
+        Nodes 5 and 6 are terminal nodes as there are no outgoing edges from either of them.
+        Every path starting at nodes 2, 4, 5, and 6 all lead to either node 5 or 6.
+        
+        Example 2:
+        Input: graph = [[1,2,3,4],[1,2],[3,4],[0,4],[]]
+        Output: [4]
+
+        Explanation:
+        Only node 4 is a terminal node, and every path starting at node 4 leads to node 4.
+
+        Constraints:
+        n == graph.length
+        1 <= n <= 104
+        0 <= graph[i].length <= n
+        0 <= graph[i][j] <= n - 1
+        graph[i] is sorted in a strictly increasing order.
+        The graph may contain self-loops.
+        The number of edges in the graph will be in the range [1, 4 * 104].
+        """;
+}
---
+++ LeetCodeTests/SolutionTests/Medium/S0802Test.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeTests.SolutionTests.Medium;
+
+public class S0802Test
+{
+    [Theory]
+    [InlineData("[[1,2],[2,3],[5],[0],[5],[],[]]", "2,4,5,6")]
+    [InlineData("[[1,2,3,4],[1,2],[3,4],[0,4],[]]", "4")]
+    [InlineData("[[],[0,2,3,4],[3],[4],[]]", "0,1,2,3,4")]
+    public void Test(string input, string expected)
+    {
+        TestReadHelper reader = new([input]);
+        TestWriteHelper<int> writer = new();
+        S0802 solver = new(reader, writer);
+        solver.Run();
+        var values = writer.LastValues!.ToList();
+
+        values.Should().BeInAscendingOrder();
+        values.Should().BeEquivalentTo(expected.Split(',').Select(int.Parse));
+    }
+}
